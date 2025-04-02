fixed gradient

optimize/general_visualiser.py
@@ -20,9 +20,11 @@ def visualiser(state, limits, freq=50, interval=100, l=1, y_limits=None, path=No
 
     point, = ax.plot(0, 0, 'ro')
     arrow = None
-    if isinstance(state.history[0], np.ndarray) and len(state.history[0]) == 2:
-        lline = ax.axvline(0, color='orange')
-        rline = ax.axvline(0, color='orange')
+
+    if len(state.history) != 0:
+        if isinstance(state.history[0], np.ndarray) and len(state.history[0]) == 2:
+            lline = ax.axvline(0, color='orange')
+            rline = ax.axvline(0, color='orange')
 
     def animate(i):
         nonlocal arrow
@@ -32,19 +34,21 @@ def visualiser(state, limits, freq=50, interval=100, l=1, y_limits=None, path=No
         guess = state.guesses[i]
         point.set_data([guess], [state.function(guess)])
 
-        if isinstance(state.history[i], np.ndarray) and len(state.history[i]) == 2:
-            left, right = state.history[i]
-            lline.set_xdata([left] * 2)
-            rline.set_xdata([right] * 2)
-            return point, lline, rline
-        elif len(state.history[i]) == 2:
-            grad_val, step = state.history[i]
-            arrow = ax.arrow(guess, state.function(guess), -grad_val * step * l, -grad_val * grad_val * step * l,
-                             head_width=0.5, head_length=0.5, fc='blue', ec='black')
-            return point, arrow
+        if len(state.history) != 0 :
+            if isinstance(state.history[i], np.ndarray) and len(state.history[i]) == 2:
+                left, right = state.history[i]
+                lline.set_xdata([left] * 2)
+                rline.set_xdata([right] * 2)
+                return point, lline, rline
+            elif len(state.history[i]) == 2:
+                grad_val, step = state.history[i]
+                arrow = ax.arrow(guess, state.function(guess), -grad_val * step * l, -grad_val * grad_val * step * l,
+                                 head_width=0.5, head_length=0.5, fc='blue', ec='black')
+                return point, arrow
+        return point
 
     ani = animation.FuncAnimation(
-        fig, animate, frames=len(state.history), repeat=True, interval=interval
+        fig, animate, frames=len(state.guesses), repeat=True, interval=interval
     )
 
     if path is not None:

optimize/gradient_descent.py
@@ -1,14 +1,23 @@
+import general_visualiser as vis
 import common
+import common.tests_function
+import optimize.wolfe_conditions
+import optimize.golden_search
+import optimize.random_search
 
 import numpy as np
-import general_visualiser as vis
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-def gradient_descent(fun, grad, step, stop, x):
+import optimize.wolfe_conditions
+
+
+def gradient_descent(fun, grad, get_next, stop, x, min_count = 10, max_count = 100):
     """
     makes gradient descent using given operators
     :param fun: is a function that we are optimizing
     :param grad: is a function that calculates gradient for fun in a given point
-    :param step: is a function that calculates the step based on function, gradient value and current state
+    :param get_next: is a function that get next value
     :param stop: is a function that determines when to stop
     :param x: is a point from which we start optimizing
     :return:
@@ -21,19 +30,20 @@ def gradient_descent(fun, grad, step, stop, x):
     res.add_guess(x)
 
     # doing steps until the end
-    while not stop(res):
+    count = 0
+    while (not stop(res) or min_count > count) and max_count > count:
+        count+=1
+        print(min_count, " ", count)
+        print(not stop(res))
         # calculate gradient
-        grad_val = grad(fun, x)
+        antigrad_val = -1 * grad(x)
+        if np.linalg.norm(antigrad_val) == 0:
+            antigrad_val = 1/count
 
-        # calculate step
-        h = step(fun, grad_val, state=res)
-
-        # doing a step
-        x = x - grad_val * h
+        print(x, "_", antigrad_val)
+        x = get_next(fun, grad, antigrad_val, x)
+        print(x)
         res.add_guess(x)
-
-        # storing context information
-        res.add_history([grad_val, h])
 
     res.success = True
     return res
@@ -177,10 +187,10 @@ def get_inv_root_step(h0):
 # absolute stoppers
 def get_stop_x_eps(eps):
     def stop(state: common.StateResult):
-        if len(state.history) < 2:
+        if len(state.guesses) < 2:
             return False
 
-        return np.linalg.norm(state.history[-1] - state.history[-2]) < eps
+        return np.linalg.norm(state.guesses[-1] - state.guesses[-2]) < eps
 
     return stop
 
@@ -197,6 +207,36 @@ def get_stop_f_eps(eps):
 
     return stop
 
+
+def get_eps_stop_determiner(eps: float):
+    """
+    eps bounds difference stopper, only works for binary search
+    :param eps: value, that determines what bounds difference it should stop
+    :return: to stop or not
+    """
+
+    def determiner(state: common.OptimizeResult) -> bool:
+        return (state.history[-1][1] - state.history[-1][0]) < eps
+
+    return determiner
+
+
+def get_next_gold(fun, grad, antigrad_val, x):
+    def func_for_gold(y):
+        return fun(x + antigrad_val * y)
+    res = optimize.golden_search.golden_search(func_for_gold, stop=get_eps_stop_determiner(0.01), bounds=[0, 1])
+    return x + antigrad_val * res.get_res()
+
+def get_next_random(fun, grad, antigrad_val, x):
+    def func_for_random(y):
+        return fun(x + antigrad_val * y)
+    res = optimize.random_search.random_search(func_for_random, stop=get_eps_stop_determiner(0.01), bounds=[0, 1])
+    return x + antigrad_val * res.get_res()
+
+def get_next_wolfe(fun, grad, antigrad_val, x):
+    res = optimize.wolfe_conditions.wolfe_conditions(fun, grad, antigrad_val, x)
+    return res.get_res()
+
 # maybe will add other later
 
 if __name__ == "__main__":
@@ -205,8 +245,23 @@ if __name__ == "__main__":
     # gr = lambda fun, x: 4 * x ** 3 - 10 * x + 2
     f = lambda x: x ** 4 / 100.0 - x ** 3 / 10 - x ** 2 / 2 + 2 * x + 5
     gr = lambda fun, x: x ** 3 / 25.0 - x ** 2 * 3.0 / 10.0 - x + 2.0
-    result = gradient_descent(f, symmetric_derivative, get_inv_root_step(1), get_stop_f_eps(0.01), 3)
-    if not result.success:
-        print("I'm sorry, no solution")
-    else:
-        vis.visualiser(result, [-12, 15], y_limits=[-40, 100], freq=200, l=5, interval=500)
+
+    for test_func in common.tests_function.functions_with_one_min:
+        lim = test_func.lim
+        result = gradient_descent(test_func.function, test_func.gradient, get_next_wolfe, get_stop_x_eps(0.01), np.array([lim[0]]))
+        print("Count of function calls: ", result.count_of_function_calls, " | result: ", result.get_res())
+        if not result.success:
+            print("didn't solve")
+        new_lim = np.array([lim[0] - lim[1] + lim[0], lim[1]])
+        vis.visualiser(result, new_lim, 500)
+    print("start functions with local min")
+    for test_func in common.tests_function.functions_with_local_min:
+        lim = test_func.lim
+        result = gradient_descent(test_func.function, test_func.gradient, get_next_wolfe, get_stop_x_eps(0.01), np.array([lim[0]]))
+        print("Count of function calls: ", result.count_of_function_calls, " | result: ", result.get_res())
+        if not result.success:
+            print("didn't solve")
+        new_lim = np.array([lim[0] - lim[1] + lim[0], lim[1]])
+        vis.visualiser(result, new_lim, 500)
+
+

optimize/multidim_gradient.py
@@ -59,7 +59,7 @@ def symmetric_derivative(fun, x, delta=1.0):
 
 # visualiser
 
-def visualiser(state: common.StateResult, lim_x, lim_y):
+def visualiser_2d(state: common.StateResult, lim_x, lim_y):
     X, Y = np.meshgrid(np.linspace(lim_x[0], lim_x[1], 200), np.linspace(lim_y[0], lim_y[1], 200))
     Z = state.function([X, Y])
 
@@ -121,14 +121,14 @@ if __name__ == "__main__":
     f = lambda x: 1 / (x[0] ** 2 + x[1] ** 2 + 0.1) + x[0] ** 2 + x[1] ** 2 + x[1] / 10
 
 
-    def gr2(fun, x, *args, **kwargs):
+    def gr2(x):
         df_dx0 = 2 * x[0] * (1 - 1 / (x[0] ** 2 + x[1] ** 2 + 0.1) ** 2)
         df_dx1 = 2 * x[1] * (1 - 1 / (x[0] ** 2 + x[1] ** 2 + 0.1) ** 2) + 1 / 10
         return np.array([df_dx0, df_dx1])
 
 
-    result = gr.gradient_descent(f, partial(forward_derivative, delta=0.05), gr.get_inv_root_step(0.1),
-                                 gr.get_stop_f_eps(0.01),
+    result = gr.gradient_descent(f, gr2, gr.get_next_wolfe,
+                                 gr.get_stop_f_eps(0.0001),
                                  np.array([-1.9, 1.5]))
     if not result.success:
         print("I'm sorry, no solution")
@@ -136,5 +136,5 @@ if __name__ == "__main__":
         print(result.get_res(), len(result.guesses))
         print(result.guesses)
         print(result.history)
-        visualiser(result, lim_x=[-2, 2], lim_y=[-2, 2])
+        visualiser_2d(result, lim_x=[-2, 2], lim_y=[-2, 2])
         visualiser_3d(result, lim_x=[-2, 2], lim_y=[-2, 2])

optimize/random_search.py
@@ -1,5 +1,6 @@
 import random
 import common.tests_function
+import general_visualiser as vis
 
 import common
 
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-def random_search(fun, max_count_step=1000000, stop=None, bounds=None):
+def random_search(fun, max_count_step=1000, stop=None, bounds=None):
     res = common.StateResult()
     # putting fun
     res.function = fun
@@ -27,9 +28,11 @@ def random_search(fun, max_count_step=1000000, stop=None, bounds=None):
 
     # you can put values you need to explain previous steps in history
     res.add_history(np.array([s, f]))
-
+    res.add_guess(s)
+    res.add_history(np.array([s, f]))
     # doing random search
-    while not stop(res) and max_count_step:
+    while not stop(res) and max_count_step > 0:
+        max_count_step -= 1
         new_point = random.uniform(s, f)
         fun_new = fun(new_point)
         res.add_function_call()
@@ -72,47 +75,6 @@ def get_eps_stop_determiner(eps: float):
 
     return determiner
 
-
-def visualiser(state: common.StateResult, limits, freq=50, path: str = None):
-    fig, ax = plt.subplots()
-
-    # setting limits
-    ax.set_xlim(limits)
-
-    t = np.arange(limits[0], limits[1], (limits[1] - limits[0]) / freq)
-
-    # drawing the base graphic
-    ax.plot(t, state.function(t))
-
-    # adding moving objects
-    point, = ax.plot(0, 0, 'ro')
-    lline = ax.axvline(0, color='orange')
-    rline = ax.axvline(0, color='orange')
-
-    # making animation function
-    def animate(i):
-        point.set_data([state.guesses[i]], [state.function(state.guesses[i])])
-        left, right = state.history[i]
-        lline.set_xdata([left] * 2)
-        rline.set_xdata([right] * 2)
-
-        return point, lline, rline
-
-    ani = animation.FuncAnimation(
-        fig,
-        animate,
-        frames=len(state.history),
-        repeat=True,
-        interval=500
-    )
-
-    if path is not None:
-        writer = animation.PillowWriter(fps=15, bitrate=1800)
-        ani.save(path, writer=writer)
-
-    plt.show()
-
-
 # example on usage of created functions
 if __name__ == "__main__":
     for test_func in common.tests_function.functions_with_one_min:
@@ -121,7 +83,7 @@ if __name__ == "__main__":
         print("Count of function calls: ", result.count_of_function_calls, " | result: ", result.get_res())
         if not result.success:
             print("didn't solve")
-        visualiser(result, lim, 200)
+        vis.visualiser(result, lim, 200)
 
     print("start functions with local min")
     for test_func in common.tests_function.functions_with_local_min:
@@ -130,4 +92,4 @@ if __name__ == "__main__":
         print("Count of function calls: ", result.count_of_function_calls, " | result: ", result.get_res())
         if not result.success:
             print("didn't solve")
-        visualiser(result, lim, 200)
+        vis.visualiser(result, lim, 200)