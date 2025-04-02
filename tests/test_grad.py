f253f5448b8b817b7e9ac34995cb174b16d26dd1
AgafonovTimofey <timagafnov@mail.ru>
2025-04-03 00:42:13 +0300

    Update tests

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -38,16 +38,56 @@ def run_func(fun, grad, get_next, stop = gr.get_stop_x_eps(0.01), point = np.arr
 def default_grad(fun):
     return fun.gradient
 
+def sym_grad(fun):
+    return gr.create_grad_from_bad_func(fun.function, gr.symmetric_derivative)
+
+def forward_grad(fun):
+    return gr.create_grad_from_bad_func(fun.function, gr.forward_derivative)
+
+def back_grad(fun):
+    return gr.create_grad_from_bad_func(fun.function, gr.backward_derivative)
+
 tests_fun = [
-    test_more_fun("gol_d", gr.get_next_gold, default_grad),
-    test_more_fun("ran_d", gr.get_next_random, default_grad),
-    test_more_fun("wol_d", gr.get_next_wolfe, default_grad),
- #   test_more_fun("con_d_1", gr.get_constant_step(1), default_grad),
- #   test_more_fun("con_d_05", gr.get_constant_step(0.5), default_grad),
- #   test_more_fun("con_d_01", gr.get_constant_step(0.1), default_grad),
-    test_more_fun("par_d_1", gr.get_partial_stuck_step(0.01, 1), default_grad),
-    test_more_fun("par_d_05", gr.get_partial_stuck_step(0.01, 0.5), default_grad),
-    test_more_fun("par_d_01", gr.get_partial_stuck_step(0.01, 0.1), default_grad)
+    test_more_fun("gol_sym", gr.get_next_gold, sym_grad),
+    test_more_fun("ran_sym", gr.get_next_random, sym_grad),
+    test_more_fun("wol_sym", gr.get_next_wolfe, sym_grad),
+    test_more_fun("con_sym_1", gr.get_constant_step(1), sym_grad),
+    test_more_fun("con_sym_05", gr.get_constant_step(0.5), sym_grad),
+    test_more_fun("con_sym_01", gr.get_constant_step(0.1), sym_grad),
+    test_more_fun("par_sym_1", gr.get_partial_stuck_step(0.01, 1), sym_grad),
+    test_more_fun("par_sym_05", gr.get_partial_stuck_step(0.01, 0.5), sym_grad),
+    test_more_fun("par_sym_01", gr.get_partial_stuck_step(0.01, 0.1), sym_grad),
+
+    test_more_fun("gol_for", gr.get_next_gold, forward_grad),
+    test_more_fun("ran_for", gr.get_next_random, forward_grad),
+    test_more_fun("wol_for", gr.get_next_wolfe, forward_grad),
+    test_more_fun("con_for_1", gr.get_constant_step(1), forward_grad),
+    test_more_fun("con_for_05", gr.get_constant_step(0.5), forward_grad),
+    test_more_fun("con_for_01", gr.get_constant_step(0.1), forward_grad),
+    test_more_fun("par_for_1", gr.get_partial_stuck_step(0.01, 1), forward_grad),
+    test_more_fun("par_for_05", gr.get_partial_stuck_step(0.01, 0.5), forward_grad),
+    test_more_fun("par_for_01", gr.get_partial_stuck_step(0.01, 0.1), forward_grad),
+
+    test_more_fun("gol_bac", gr.get_next_gold, back_grad),
+    test_more_fun("ran_bac", gr.get_next_random, back_grad),
+    test_more_fun("wol_bac", gr.get_next_wolfe, back_grad),
+    test_more_fun("con_bac_1", gr.get_constant_step(1), back_grad),
+    test_more_fun("con_bac_05", gr.get_constant_step(0.5), back_grad),
+    test_more_fun("con_bac_01", gr.get_constant_step(0.1), back_grad),
+    test_more_fun("par_bac_1", gr.get_partial_stuck_step(0.01, 1), back_grad),
+    test_more_fun("par_bac_05", gr.get_partial_stuck_step(0.01, 0.5), back_grad),
+    test_more_fun("par_bac_01", gr.get_partial_stuck_step(0.01, 0.1), back_grad),
+
+    test_more_fun("gol_def", gr.get_next_gold, default_grad),
+    test_more_fun("ran_def", gr.get_next_random, default_grad),
+    test_more_fun("wol_def", gr.get_next_wolfe, default_grad),
+    test_more_fun("con_def_1", gr.get_constant_step(1), default_grad),
+    test_more_fun("con_def_05", gr.get_constant_step(0.5), default_grad),
+    test_more_fun("con_def_01", gr.get_constant_step(0.1), default_grad),
+    test_more_fun("par_def_1", gr.get_partial_stuck_step(0.01, 1), default_grad),
+    test_more_fun("par_def_05", gr.get_partial_stuck_step(0.01, 0.5), default_grad),
+    test_more_fun("par_def_01", gr.get_partial_stuck_step(0.01, 0.1), default_grad)
+
 ]
 
 print("Test 2d with one min")
@@ -57,8 +97,8 @@ for t_fun in test_fun.functions_with_one_min_2d:
     count2-=1
     print(count2)
     for f in tests_fun:
-        f.add_new_tests(20)
-    for _ in range(20):
+        f.add_new_tests(100)
+    for _ in range(100):
         x = random.uniform(lim[0], lim[1])
         y = random.uniform(lim[0], lim[1])
         for f in tests_fun:
